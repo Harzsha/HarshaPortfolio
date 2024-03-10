185fc0d510d611d8ec4fcc66ffb1d9bef620f08a
Harsha <harshagowtham30@gmail.com>
2024-03-10 11:24:59 +0530

    issue fixes!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +9,11 @@ import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
 export class HomeComponent implements OnInit {
 
   constructor(
-    private analyticsService: AnalyticsService,
   ) {
   }
 
   ngOnInit(): void {
-    this.analyticsService.sendAnalyticPageView("/inicio", "Se entro a inicio")
+    
   }
 
 
